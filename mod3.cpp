229178d2985c13933083f5c164b07f5667faa7cc
joshigagan4 <59817693+joshigagan4@users.noreply.github.com>
2020-10-12 18:13:11 +0530

    Update mod3.cpp

--- mod3.cpp
+++ mod3.cpp
@@ -1,24 +1,5 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    string n ;
-    cout<<"Enter the string \n";
-    cin>>n;
-    int count1=0,count0=0;
-    for(int i =0;i<n.size();i++){
-        if(n[i]=='1'){
-            count1++;
-        }else{
-        count0++;}
-    }
-    if(count1%3==0||count0%3==0){
-        cout<<"Accepted \n";
-    }
-    else{
-        cout<<"Not Accepted \n";
-    }
-
-
-}
+s = input("Enter an expression:")
+if s.count('1') % 3 == 0 or s.count('0') % 3 == 0:
+    print("Accepted")
+else:
+    print("Rejected")
